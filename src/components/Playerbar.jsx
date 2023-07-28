c66c3db9e2345ea0c7b42dd29e24fa439c67653a
phamthong3107 <phamthongtruykich@gmail,com>
2023-07-29 04:05:30 +0700

    edit xiu

--- src/components/Playerbar.jsx
+++ src/components/Playerbar.jsx
@@ -5,21 +5,32 @@ import { Songs } from '../Context';
 
 export default function Playerbar() {
   const {Artist, song, handleSetsong} = useContext(Songs)
+  const songNext = song + 1
   const handleClickNext = () => {
-    handleSetsong(song + 1)
+    handleSetsong(songNext)
   }
   const handleClickPre = () => {
     handleSetsong(song - 1)
   }
   return (
-    <div className='player-bar'>
-        <AudioPlayer 
+    <div className='player-bar flex'>
+      <div className="song_now flex">
+        <img src={Artist.songs[song].img} className='w-[70px] h-[70px] m-[10px]' />
+        <div className="song_now-content flex-col">
+          <h2 className='font-bold'>{Artist.songs[song].nameSong}</h2>
+          <span className='text-[14px]'>{Artist.nameAuthor}</span>
+        </div>
+        <img src="/img/wave.gif" className='w-[30px] h-[30px] mt-[30px] mr-3' />
+      </div>
+      <AudioPlayer 
         src={Artist.songs[song].nameFile} 
         showSkipControls={true}
         onClickNext={handleClickNext}
         onClickPrevious={handleClickPre}
-       />
-        
+      />
+      <div className="icon-list bg-white">
+        <img src="/img/list.gif" className='w-[50px] h-[50px] m-5' />
+      </div>
     </div>
     
   )
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -4,13 +4,14 @@ import { Songs } from '../Context'
 export default function Rightbar() {
     const {DataSongs, handlesetArtist} = useContext(Songs) 
     const [idArtist, setidArtist] = useState(0)
+   
     const handleArtistNow = (idArtist) =>{
         setidArtist(idArtist)
         handlesetArtist(idArtist)
     }
   return (
     <div className="right-bar ml-[20px]">
-            <h3 className="font-bold text-[18px] mt-[40px]">Recent Singers</h3>
+            <h3 className="font-bold text-[18px]">Recent Singers</h3>
             <div className="list-artist">
                 {DataSongs.map((artist) => (
                 <div className={`list-artist_item flex ${idArtist === artist.id && 'bg-white'}` }
@@ -21,6 +22,19 @@ export default function Rightbar() {
                         <div className="list-artist-nameUpdate">{artist.update}</div>
                     </div>
                 </div>
+                ))}
+            </div>
+            <div className="list-another">
+                <h3 className="font-bold text-[18px] mt-[10px]">Another songs</h3>
+                {DataSongs.map((artist) => (
+                <div className="list-artist_item flex">
+                    <img src={artist.songs[0].img} alt=""/>
+                    <div className="list-artist_name flex-col">
+                        <div className="list-artist-nameSong">{artist.songs[0].nameSong}</div>
+                        <div className="list-artist-nameUpdate">{artist.nameAuthor}</div>
+                    </div>
+                </div>
+                
                 ))}
             </div>
         </div>
